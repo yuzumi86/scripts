ab817e7f33849d201fe8574479af9ca087f7939e
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-15 07:50:41 +0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+# Remove local_manifests directory
+rm -rf .repo/local_manifests
+
+# Clone the source
+repo init -u https://github.com/CipherOS/android_manifest.git -b twelve-L
+
+# Clone the manifest repository
+git clone https://github.com/yuzumi86/local_manifest --depth 1 -b main .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
+#Extra
+rm -rf prebuilts/clang/host/linux-x86/clang-r450784d
+git clone https://gitlab.com/ImSurajxD/clang-r450784d -b master prebuilts/clang/host/linux-x86/clang-r450784d
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Jakarta
+
+# Set up the build environment
+source build/envsetup.sh
+repo forall -c 'git lfs install && git lfs pull && git lfs checkout'
+
+# Lunch for even device
+lunch cipher_eveb-userdebug
+
+# Build for even device
+mka bacon
