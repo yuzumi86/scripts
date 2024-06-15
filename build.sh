ed59187b924afcb55b03addda984e8ae0b0329d8
yuzumi <157438148+yuzumi86@users.noreply.github.com>
2024-06-15 10:23:29 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
 # Remove local_manifests directory
-rm -rf .repo/local_manifests
 
 # Clone the source
 repo init -u https://github.com/CipherOS/android_manifest.git -b twelve-L
 
 # Clone the manifest repository
-git clone https://github.com/yuzumi86/local_manifest --depth 1 -b main .repo/local_manifests
+rm -rf device/realme/even
+git clone https://github.com/yuzumi86/device_realme_even --depth 1 -b master ./device/realme/even
+rm -rf kernel/realme/even
+git clone --recurse-submodules --depth=1 https://github.com/yuzumi86/kernel_realme_even -b OSS-KSU-R ./kernel/realme/even
+rm -rf vendor/realme/even
+git clone --depth=1 https://github.com/cumaRull/vendor_realme_even -b rmui2 ./vendor/realme/even
+rm -rf packages/apps/RealmeParts
+git clone --depth=1 https://github.com/Realme-C25-Series-Development/android_packages_apps_RealmeParts -b aosp12 ./packages/apps/RealmeParts
+rm -rf vendor/realme/even-ims
+git clone --depth=1 https://github.com/eun0115/vendor_oplus_even-ims -b twelve-rmui2 ./vendor/realme/even-ims
 
 # Sync the repositories
 /opt/crave/resync.sh
